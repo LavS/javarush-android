f69e92154fb265cd371243edc52bc99494e92918
Lavsexpert <lavrov-sergey@yandex.ru>
2018-06-04 22:56:35 +0300

    Подключили кнопку и написан первый код

--- JustJava/app/src/main/java/ru/javarush/justjava/MainActivity.java
+++ JustJava/app/src/main/java/ru/javarush/justjava/MainActivity.java
@@ -1,13 +1,41 @@
+/**
+ * IMPORTANT: Make sure you are using the correct package name. 
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+
 package ru.javarush.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
+/**
+ * Это приложение показывает форму заказа кофе.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Этот метод вызывается при нажатии кнопки.
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /**
+     * Этот метод отображает выбранное количество на экран.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
