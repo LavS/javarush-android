899ee34988568a45bb6cccd559597d5213047e0a
Lavsexpert <lavrov-sergey@yandex.ru>
2018-06-05 12:10:26 +0300

    7.27 - теперь работает подсчет очков для обоих команд

--- CourtCounter/app/src/main/java/ru/javarush/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/ru/javarush/courtcounter/MainActivity.java
@@ -8,12 +8,14 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 
     /**
@@ -21,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -37,5 +47,20 @@ public class MainActivity extends AppCompatActivity {
     public void addThreeToTeamA(View view){
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
+    }
+
+    public void addOneToTeamB(View view){
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoToTeamB(View view){
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addThreeToTeamB(View view){
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
     }
 }
