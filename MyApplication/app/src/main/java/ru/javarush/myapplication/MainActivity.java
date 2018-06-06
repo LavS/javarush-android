88cb8f157b556a0e6b0486bdb1c76b7c7874c232
Lavsexpert <lavrov-sergey@yandex.ru>
2018-06-06 14:05:19 +0300

    8.18 - изменен способ вывода экрана. Вместо загрузки макета - создаётся элемент TextView, заполняется и выводится.

--- MyApplication/app/src/main/java/ru/javarush/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/ru/javarush/myapplication/MainActivity.java
@@ -2,12 +2,18 @@ package ru.javarush.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText("Тест");
+        setContentView(textView);
     }
 }
